afa168cf0ac3e1d29a8dc0455831d1495663f53c
weiqiang <weiqiang@growingio.com>
2020-01-16 17:09:53 +0800

    update cmd

--- devops.go
+++ devops.go
@@ -1,22 +1,51 @@
-/*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package main
 
-import "github.com/weiqiang333/devops/cmd"
+import (
+  "flag"
+  "fmt"
+  "os"
+
+  "github.com/mitchellh/go-homedir"
+  "github.com/spf13/viper"
+
+  "github.com/weiqiang333/devops/web"
+)
+
 
 func main() {
-  cmd.Execute()
+  web.Web()
+}
+
+func init()  {
+  cfgFile := flag.String("config", "", "config file (default is $HOME/.devops.yaml)")
+  address := flag.String("address", "0.0.0.0:8080", "devops run listening address (default 0.0.0.0:8080)")
+  flag.Parse()
+
+  viper.SetDefault("address", address)
+  initConfig(*cfgFile)
+}
+
+func initConfig(cfgFile string) {
+  if cfgFile != "" {
+    // Use config file from the flag.
+    viper.SetConfigFile(cfgFile)
+  } else {
+    // Find home directory.
+    home, err := homedir.Dir()
+    if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+    }
+
+    // Search config in home directory with name ".devops" (without extension).
+    viper.AddConfigPath(home)
+    viper.SetConfigName(".devops")
+  }
+
+  viper.AutomaticEnv() // read in environment variables that match
+
+  // If a config file is found, read it in.
+  if err := viper.ReadInConfig(); err == nil {
+    fmt.Println("Using config file:", viper.ConfigFileUsed())
+  }
 }
